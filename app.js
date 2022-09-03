b19041f8c3a318347f6291eaa15db94d45cc1ec9
MacBook <macbook@MacBooks-MacBook-Air.local>
2022-09-03 09:07:24 +0600

    cotagory add

---
+++ app.js
@@ -0,0 +1,24 @@
+const loadCategoris = async() => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const data = await res.json();
+    showCategoris(data.data.news_category)
+
+
+}
+const showCategoris = (catagoris) => {
+    console.log(catagoris)
+    const newsItem = document.getElementById('news-item')
+    newsItem.classList.add('d-flex')
+    catagoris.forEach(catagory => {
+        const li = document.createElement('li')
+        li.innerHTML = `
+        <a>${catagory. category_name}</a>
+
+        `
+        newsItem.appendChild(li)
+
+    });
+
+}
+loadCategoris()
